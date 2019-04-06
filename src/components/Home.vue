5548575c98996a2e0376b9c9d942da664ba7754e
BJBaardse <beer.baardse@gmail.com>
2019-04-06 15:48:10 +0200

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <navbar></navbar>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/components/Login'
+import Home from '@/components/Home'
+
+Vue.use(Router)
+export const router = new Router({
+  mode: 'history',
+  routes: [
+    { path: '/', component: Login },
+    { path: '/home', component: Home },
+    // otherwise redirect to home
+    { path: '*', redirect: '/' }
+  ]
+})
+router.beforeEach((to, from, next) => {
+  // redirect to login page if not logged in and trying to access a restricted page
+  const publicPages = ['/'];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem('token');
+
+  if (authRequired && !loggedIn) {
+    return next('/');
+  }
+
+  next();
+})
